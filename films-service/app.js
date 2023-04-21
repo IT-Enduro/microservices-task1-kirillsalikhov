87ae633013e98d3fcb41946bb8ddf7059de5c962
kirill <kirill.salikhov@gmail.com>
2023-04-21 20:18:40 +0400

    build: make nodejs process stop faster

--- films-service/app.js
+++ films-service/app.js
@@ -2,10 +2,17 @@ const Koa = require('koa');
 
 const port = 8080;
 
-new Koa()
+const app = new Koa()
     .use(async ctx => {
         ctx.body = `Hello`;
     })
     .listen(port, () => {
         console.log(`Ready to receive requests on ${port}`);
-    })
+    });
+
+['SIGINT', 'SIGTERM'].forEach((signal) => {
+    process.on(signal, function() {
+        console.log('Closing server');
+        app.close(process.exit);
+    });
+});
