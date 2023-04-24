f0889bf98917cc7a8f250923840c7f70a0f3d60b
kirill <kirill.salikhov@gmail.com>
2023-04-25 00:08:26 +0400

    feat: add generated api client, add shared-libs

---
+++ shared-libs/api-client/index.js
@@ -0,0 +1,7 @@
+const { FilmsServiceApi } = require("./generated/dist");
+
+exports.filmsApi = new FilmsServiceApi({ basePath: 'http://films-service:8070' });
+
+exports.cinemaApi = new CinemaServiceApi({ basePath:'http://cinema-service:8060' });
+
+exports.ticketsApi = new TicketServiceApi({ basePath:'http://cinema-service:8080' })
